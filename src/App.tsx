4305fd0897e1ab5a23505c6e54771c20bb35f881
Sofiia13 <sophiyastanish@gmail.com>
2025-08-05 15:29:49 +0300

    fix issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 import { Tabs } from './components/Tabs';
 import { NotFound } from './components/NotFound';
 import { Home } from './components/Home';
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { Link, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -12,48 +12,53 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              'navbar-item' + (isActive ? ' is-active' : '')
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/tabs"
-            className={({ isActive }) =>
-              'navbar-item' + (isActive ? ' is-active' : '')
-            }
-          >
-            Tabs
-          </NavLink>
+export const App = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={
+                'navbar-item' + (location.pathname === '/' ? ' is-active' : '')
+              }
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={
+                'navbar-item' +
+                (location.pathname.startsWith('/tabs') ? ' is-active' : '')
+              }
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="tabs">
+              <Route index element={<Tabs tabs={tabs} />} />
+              <Route path=":tabId" element={<Tabs tabs={tabs} />} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path="tabs">
-            <Route index element={<Tabs tabs={tabs} />} />
-            <Route path=":tabId" element={<Tabs tabs={tabs} />} />
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
